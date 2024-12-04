b5620c3e02805ddc6c8ee8c3b7663af92d99aebd
Ephaltes <ngoc.lam@live.at>
2024-12-04 22:01:38 +0100

    Day Four Puzzle refactor

--- AOC.Four/Board.cs
+++ AOC.Four/Board.cs
@@ -30,20 +30,33 @@ public class Board
 
     public int GetWordCountInBoard(char[] wordToFind)
     {
-        _verticalBoundary = _grid.Length;
+        char crossCharacter = wordToFind[0];
 
-        int occurrences = 0;
-        for (int y = 0; y < _verticalBoundary; y++)
-        {
-            _horizontalBoundary = _grid[0].Length;
-            for (int x = 0; x < _horizontalBoundary; x++)
-                occurrences += GetWordCount(new Position(y, x), wordToFind);
-        }
+        List<Direction> directionsToSearch = Enum.GetValues<Direction>().ToList();
 
-        return occurrences;
+        List<Position> crossCharacterPositions = GetWordPositions(wordToFind, crossCharacter, directionsToSearch);
+
+        return crossCharacterPositions.Count;
     }
 
     public int GetCrossedWordCountInBoard(char[] wordToFind, char crossCharacter)
+    {
+        List<Direction> directionsToSearch =
+        [
+            Direction.NortWest,
+            Direction.NorthEast,
+            Direction.SouthWest,
+            Direction.SouthEast
+        ];
+
+        List<Position> crossCharacterPositions = GetWordPositions(wordToFind, crossCharacter, directionsToSearch);
+
+        return crossCharacterPositions
+               .CountBy(position => (position.X, position.Y))
+               .Count(group => group.Value / 2 > 0);
+    }
+
+    private List<Position> GetWordPositions(char[] wordToFind, char crossCharacter, List<Direction> directionsToSearch)
     {
         _verticalBoundary = _grid.Length;
 
@@ -53,40 +66,27 @@ public class Board
             _horizontalBoundary = _grid[0].Length;
             for (int x = 0; x < _horizontalBoundary; x++)
             {
-                List<Position>? crossCharacterLinePositions = GetCrossWordCount(new Position(y, x), wordToFind, crossCharacter);
+                if (_grid[y][x] != wordToFind[0])
+                    continue;
 
-                if (crossCharacterLinePositions is not null)
-                    crossCharacterPositions.AddRange(crossCharacterLinePositions);
+                List<Position> crossCharacterLinePositions =
+                    SearchDirections(new Position(y, x), wordToFind, crossCharacter, directionsToSearch);
+
+                crossCharacterPositions.AddRange(crossCharacterLinePositions);
             }
         }
 
-        return crossCharacterPositions
-               .CountBy(position => (position.X, position.Y))
-               .Count(group => group.Value / 2 > 0);
+        return crossCharacterPositions;
     }
 
-    private List<Position>? GetCrossWordCount(Position position, char[] wordToFind, char crossCharacter)
-    {
-        if (_grid[position.Y][position.X] != wordToFind[0])
-            return null;
-
-        List<Direction> directionsToSearch =
-        [
-            Direction.NortWest,
-            Direction.NorthEast,
-            Direction.SouthWest,
-            Direction.SouthEast
-        ];
-        return SearchCrossDirections(position, wordToFind, crossCharacter, directionsToSearch);
-    }
-
-    private List<Position> SearchCrossDirections(Position position, char[] wordToFind, char crossCharacter, List<Direction> directionsToSearch)
+    private List<Position> SearchDirections(
+        Position position, char[] wordToFind, char crossCharacter, List<Direction> directionsToSearch)
     {
         List<Position> crossCharacterPositions = [];
 
         foreach (Direction direction in directionsToSearch)
         {
-            Position? foundPosition = SearchInCross(position, direction, wordToFind, crossCharacter);
+            Position? foundPosition = SearchDirection(position, direction, wordToFind, crossCharacter);
 
             if (foundPosition is not null)
                 crossCharacterPositions.Add(foundPosition.Value);
@@ -95,26 +95,23 @@ public class Board
         return crossCharacterPositions;
     }
 
-    private Position? SearchInCross(Position position, Direction direction, char[] wordToFind, char crossCharacter)
+    private Position? SearchDirection(Position position, Direction direction, char[] wordToFind, char crossCharacter)
     {
         Position directionPath = _directionMapping[direction];
-        Position newPosition = position + directionPath;
+        Position newPosition = position;
         Position? characterPosition = null;
 
-        for (int i = 1; i < wordToFind.Length; i++)
+        foreach (char currentCharacterToFind in wordToFind)
         {
-            if (newPosition.X >= _horizontalBoundary ||
-                newPosition.Y >= _verticalBoundary ||
-                newPosition.X < 0 ||
-                newPosition.Y < 0)
+            if (IsOutOfBound(newPosition))
                 return null;
 
-            char character = _grid[newPosition.Y][newPosition.X];
+            char gridCharacter = _grid[newPosition.Y][newPosition.X];
 
-            if (character != wordToFind[i])
+            if (gridCharacter != currentCharacterToFind)
                 return null;
 
-            if (character == crossCharacter)
+            if (gridCharacter == crossCharacter)
                 characterPosition = newPosition;
 
             newPosition += directionPath;
@@ -123,39 +120,11 @@ public class Board
         return characterPosition;
     }
 
-    private int GetWordCount(Position position, char[] wordToFind)
+    private bool IsOutOfBound(Position newPosition)
     {
-        if (_grid[position.Y][position.X] != wordToFind[0])
-            return 0;
-
-        return SearchInAllDirections(position, wordToFind);
-    }
-
-    private int SearchInAllDirections(Position position, char[] wordToFind)
-    {
-        return Enum.GetValues<Direction>()
-                   .Sum(direction => SearchInDirection(position, direction, wordToFind));
-    }
-
-    private int SearchInDirection(Position position, Direction direction, char[] wordToFind)
-    {
-        Position directionPath = _directionMapping[direction];
-        Position newPosition = position + directionPath;
-
-        for (int i = 1; i < wordToFind.Length; i++)
-        {
-            if (newPosition.X >= _horizontalBoundary ||
-                newPosition.Y >= _verticalBoundary ||
-                newPosition.X < 0 ||
-                newPosition.Y < 0)
-                return 0;
-
-            if (_grid[newPosition.Y][newPosition.X] != wordToFind[i])
-                return 0;
-
-            newPosition += directionPath;
-        }
-
-        return 1;
+        return newPosition.X >= _horizontalBoundary ||
+               newPosition.Y >= _verticalBoundary ||
+               newPosition.X < 0 ||
+               newPosition.Y < 0;
     }
 }
